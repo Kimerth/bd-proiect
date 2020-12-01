6fcbf6b3504119412f35656ebf2be834e05117e1
Kimerth <motfoleas@yahoo.com>
2020-12-01 16:31:43 +0200

    connection pool

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import cx_Oracle
 from configparser import ConfigParser
 
-if __name__ == "__main__":
+def connect():
     config = ConfigParser()
     config.read("db.config")
 
     db_cfg = config['oracle.sql']
-    conn = cx_Oracle.connect(db_cfg['usr'], db_cfg['pwrd'], db_cfg['conn'])
-    print("Database version:", conn.version)
-    conn.close()
+    pool = cx_Oracle.SessionPool(db_cfg['usr'], db_cfg['pwrd'], db_cfg['conn'],
+                             min = 2, max = 5, increment = 1, threaded = True)
+    return pool
+
+def cursor(conn):
+    csr = conn.cursor()
+    return csr
+
+if __name__ == "__main__":
+    pool = connect()
+    conn = pool.acquire()
+    print("Database version:", pool.acquire().version)
