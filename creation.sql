204605f8b7feb4b7679c6f627b2a6e4614b56a32
Kimerth <motfoleas@yahoo.com>
2020-12-12 16:40:36 +0200

    fixed relationships

--- creation.sql
+++ creation.sql
@@ -1,7 +1,16 @@
+CREATE TABLE `Buildings`
+(
+ `BuildingID`      int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
+ `Address`         varchar(45) NOT NULL ,
+ 'City'            varchar(45) NOT NULL ,
+ `Zipcode`         varchar(45) NOT NULL ,
+);
+
 CREATE TABLE `Departments`
 (
  `DepartmentID`    int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
  `Department Name` varchar(45) NOT NULL ,
+ `BuildingID`      int NOT NULL FOREIGN KEY REFERENCES `Buildings` (`BuildingID`) UNIQUE ,
  `Website`         varchar(45) NULL ,
 );
 
@@ -13,12 +22,12 @@ CREATE TABLE `Researchers`
  `DepartmentID`  int NOT NULL FOREIGN KEY REFERENCES `Departments` (`DepartmentID`),
  `Email Address` varchar(45) NOT NULL UNIQUE,
  `Phone Number`  varchar(45) NOT NULL UNIQUE,
- `Role`          varchar(45) NOT NULL CHECK (Role = 'tehnician' OR Role = 'laborant' OR Role = 'cercetator'),
+ `Role`          varchar(45) NOT NULL CHECK (Role = 'tehnician' OR Role = 'laborant' OR Role = 'cercetator') ,
 );
 
 CREATE TABLE `Tools`
 (
- `DeviceID`       int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
+ `ToolID`         int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
  `Manufacturer`   varchar(45) NOT NULL ,
  `Model name`     varchar(45) NOT NULL ,
  `Serial Number`  varchar(45) NOT NULL UNIQUE,
@@ -28,26 +37,28 @@ CREATE TABLE `Tools`
 CREATE TABLE `Experiments`
 (
  `ExperimentID` int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
- `UserID`       int NOT NULL FOREIGN KEY REFERENCES `Researchers` (`UserID`),
- `DeviceID`     int NOT NULL  FOREIGN KEY REFERENCES `Tools` (`DeviceID`),
  `Title`        varchar(45) NOT NULL ,
  `Description`  varchar(512) NOT NULL ,
  `Theory`       varchar(512) NOT NULL ,
 );
 
+CREATE TABLE `ExperimentsResearchersRelation`
+(
+ `ExperimentID` int NOT NULL FOREIGN KEY REFERENCES `Experiments` (`ExperimentID`) ,
+ `UserID`       int NOT NULL FOREIGN KEY REFERENCES `Researchers` (`UserID`) ,
+);
+
+CREATE TABLE `ExperimentsToolsRelation`
+(
+ `ExperimentID` int NOT NULL FOREIGN KEY REFERENCES `Experiments` (`ExperimentID`) ,
+ `ToolID`       int NOT NULL FOREIGN KEY REFERENCES `Tools` (`ToolID`) ,
+);
+
 CREATE TABLE `Results`
 (
  `ResultID`     int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
- `ExperimentID` int NOT NULL FOREIGN KEY REFERENCES `Experiments` (`ExperimentID`),
+ `ExperimentID` int NOT NULL FOREIGN KEY REFERENCES `Experiments` (`ExperimentID`) ,
  `Remarks`      varchar(512) NOT NULL ,
  `Observations` varchar(512) NOT NULL ,
  `Description`  varchar(512) NOT NULL ,
-);
-
-CREATE TABLE `Papers`
-(
- `RaportID`     int NOT NULL PRIMARY KEY NUMBER GENERATED ALWAYS AS IDENTITY ,
- `ExperimentID` int NOT NULL FOREIGN KEY REFERENCES `Experiments` (`ExperimentID`),
- `Abstract`     varchar(2048) NOT NULL ,
- `Link to pdf`  varchar(45) NULL ,
 );
