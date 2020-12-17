c7d0b77294614a85972b82d4578f6abe7f25574f
Kimerth <motfoleas@yahoo.com>
2020-12-17 15:20:40 +0200

    including OO and OM relationships in table display

--- interface/oracleDB.py
+++ interface/oracleDB.py
@@ -15,27 +15,50 @@ def cursor(pool):
 
 def get_users(pool):
     csr = cursor(pool)
-    csr.execute('SELECT * FROM "Researchers"')
+    csr.execute("""
+        SELECT r."UserID" as "ID", r."First Name", r."Last Name", d."Department Name" as "Department", r."Email Address", r."Phone Number", r."Role" 
+        FROM "Researchers" r, "Departments" d
+        WHERE r."DepartmentID" = d."DepartmentID"
+        ORDER BY r."UserID"
+    """)
     return csr
 
 def get_tools(pool):
     csr = cursor(pool)
-    csr.execute('SELECT * FROM "Tools"')
+    csr.execute("""
+        SELECT t."ToolID" as "ID", t."Manufacturer", t."Model name", t."Serial Number", COALESCE(t."Specifications", '') "Specifications"
+        FROM "Tools" t
+        ORDER BY t."ToolID"
+    """)
     return csr
 
 def get_experiments(pool):
     csr = cursor(pool)
-    csr.execute('SELECT * FROM "Experiments"')
+    csr.execute("""
+        SELECT e."ExperimentID" as "ID", e."Title", e."Description", e."Theory"
+        FROM "Experiments" e
+        ORDER BY e."ExperimentID"
+    """)
     return csr
 
 def get_results(pool):
     csr = cursor(pool)
-    csr.execute('SELECT * FROM "Results"')
+    csr.execute("""
+        SELECT r."ResultID" as "ID", e."Title" as "Experiment", r."Remarks", r."Observations", r."Description"
+        FROM "Results" r, "Experiments" e
+        WHERE r."ExperimentID" = e."ExperimentID"
+        ORDER BY r."ResultID"
+    """)
     return csr
 
 def get_departments(pool):
     csr = cursor(pool)
-    csr.execute('SELECT * FROM "Departments"')
+    csr.execute("""
+        SELECT d."DepartmentID" as "ID", d."Department Name", b."Address" || ' ' || b."City" || ' ' || b."Zipcode" "Address", COALESCE(d."Website", '') "Website"
+        FROM "Departments" d, "Buildings" b
+        WHERE d."BuildingID" = b."BuildingID"
+        ORDER BY d."DepartmentID"
+    """)
     return csr
 
 def get_buildings(pool):
---
+++ static/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('myBtn').addEventListener('click', () => {
+            document.querySelector('.bg-modal').style.display ='flex';
+    });
+    document.querySelector('.close').addEventListener('click', () => {
+        document.querySelector('.bg-modal').style.display ='none';
+    });
+});
